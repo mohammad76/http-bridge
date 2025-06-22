e5672f7eb09e7bef8522803ebfb1f772d29b6c6d
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2025-06-22 07:38:16 +0330

    update

--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ import requests
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import smtplib
+from email.mime.text import MIMEText
 
 
 @csrf_exempt
@@ -31,4 +33,44 @@ def proxy_view(request):
             return HttpResponse(resp.content, status=resp.status_code, content_type=content_type)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Only POST supported'}, status=405)
+
+
+@csrf_exempt
+def send_mail_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        subject = data.get('subject', '')
+        a_from = data.get('from', '')
+        to = data.get('to', '')
+        body = data.get('body', '')
+        SMTP_SERVER = data.get('smtp_server', '')
+        SMTP_PORT = data.get('smtp_port', '')
+        USERNAME = data.get('username', '')
+        PASSWORD = data.get('password', '')
+        USE_TLS = True  # اگر روی 465 زدی کنش False و پورت رو بزار 465
+
+        if not subject or not to or not body:
+            return JsonResponse({'error': 'Missing subject, to, or body parameter'}, status=400)
+
+        msg = MIMEText('سلام! این یک ایمیل تستی از پایتون است.')
+        msg['Subject'] = 'تست SMTP از Python'
+        msg['From'] = a_from
+        msg['To'] = to
+
+        try:
+            if USE_TLS:
+                server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT, timeout=10)
+                server.ehlo()
+                server.starttls()
+                server.login(USERNAME, PASSWORD)
+            else:
+                server = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT, timeout=10)
+                server.login(USERNAME, PASSWORD)
+
+            server.sendmail(a_from, [to], msg.as_string())
+            server.quit()
+            return JsonResponse({'msg': 'ok'}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': 'prm in send'}, status=500)
     return JsonResponse({'error': 'Only POST supported'}, status=405)
